ecc86e0c3791372279b8baf2f4797d75e7acf404
SamSam <sam40230@gmail.com>
2023-10-24 15:01:57 +0200

    Bataille navale v0.1 (java)

---
+++ src/fr/fms/Bataille_navale.java
@@ -0,0 +1,22 @@
+package fr.fms;
+import java.util.Scanner;
+
+public class Bataille_navale {
+
+	public static void main(String[] args) {
+	       
+
+	    	  int grid[][] = new int[10][10];
+
+	    	  for (int i = 0;i < grid.length; i++) {
+	    		   for (int j = 0;j < grid[i].length;j++) {
+	    			   grid[i][j] = 0;
+	    			   System.out.println(grid[i][j] + " ");
+	    
+	    	        }   
+	    	  }
+	    	 
+	    	  
+	}
+	
+}
